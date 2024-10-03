7f0136498cb6f308549057d7272e07232cba5350
helmut <helmut.andrey@gmail.com>
2024-10-03 14:58:08 +0500

    Немного улучшили....но все еще понубски

--- renamer_test.py
+++ renamer_test.py
@@ -4,8 +4,10 @@ current_dir = os.getcwd()
 print('''Привет это программа renamer v1, поможет массово исправить расширение файлов в текущей директории''')
 
 
+
+
 file_ext_current = '.jpg'
-change_ext = input(f'Текущее расширение файла {file_ext_current}, будем менять? y/n: ')
+change_ext = input(f'Текущее расширение файла для установки {file_ext_current}, будем менять? y/n: ')
 if change_ext == 'y':
     new_ext = input('Введите расширение в виде ".jpg"')
 
@@ -25,6 +27,17 @@ list_file = []
 for name in files_and_dir():
     if not name in uninclude:
         list_file.append(name)
+
+
+print('Список файлов для переименования и пример первого файла: ')
+for file in list_file:
+    print(file)
+
+print(f'Файлы будут иметь след вид: {list_file[-1] + file_ext_current}')
+change_return = input('Продолжаем?.....y/n: ')
+
+if change_return == 'n':
+    exit()
 
 #переименование os.rename(original_name, new_name)
 
